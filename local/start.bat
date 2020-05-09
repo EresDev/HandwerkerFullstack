d45bb689c4d7df505a66c505080308133565df97
EresDev <hi@eresdev.com>
2020-05-10 01:12:00 +0500

    Files cleanup.

--- local/start.bat
+++
@@ -1,9 +0,0 @@
-REM Scripts to end programs after development, e.g. stop server, mysql, etc.
-
-REM Install MariaDB from its ZIP file
-REM bin/mysql_install_db.exe --datadir=c:\mydatabase --service=MyDB --password=root_password
-
-cd X:\Programs\nginx\nginx-1.18.0
-start-php-fcgi.bat
-start nginx
-cd X:\Projects\HandwerkerFullstack\local
--- run.bat
+++ run.bat
@@ -5,9 +5,9 @@ X:
 cd X:\Programs\mariadb\mariadb-10.4.12-winx64\bin
 mysqladmin -u root shutdown
 
+
 start mysqld --defaults-file=X:\Programs\mariadb\mariadb-10.4.12-winx64\data\my.ini
 REM mysql -u root
-
 cd X:\Programs\nginx\nginx-1.18.0
 start nginx
 cd X:\Projects\HandwerkerFullstack
