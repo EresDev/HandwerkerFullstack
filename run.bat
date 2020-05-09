74de27d67c8fa4f8ae4fa1ec283f016498a14d27
EresDev <hi@eresdev.com>
2020-05-10 01:11:37 +0500

    Start mysql server command added

--- run.bat
+++ run.bat
@@ -1,6 +1,12 @@
 REM Scripts to start programs before development, e.g. start server, mysql, etc.
 
 TASKKILL /F /IM "nginx*"
+X:
+cd X:\Programs\mariadb\mariadb-10.4.12-winx64\bin
+mysqladmin -u root shutdown
+
+start mysqld --defaults-file=X:\Programs\mariadb\mariadb-10.4.12-winx64\data\my.ini
+REM mysql -u root
 
 cd X:\Programs\nginx\nginx-1.18.0
 start nginx
