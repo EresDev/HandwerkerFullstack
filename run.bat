5949e8bcfc4a6a15b052d568fbc39f748de30899
EresDev <hi@eresdev.com>
2020-05-10 22:52:47 +0500

    do not change directories for some commands.

--- run.bat
+++ run.bat
@@ -1,17 +1,16 @@
 REM Scripts to start programs before development, e.g. start server, mysql, etc.
 
 TASKKILL /F /IM "nginx*"
-X:
-cd X:\Programs\mariadb\mariadb-10.4.12-winx64\bin
-mysqladmin -u root shutdown
+X:\Programs\mariadb\mariadb-10.4.12-winx64\bin\mysqladmin -u root shutdown
 
 
-start mysqld --defaults-file=X:\Programs\mariadb\mariadb-10.4.12-winx64\data\my.ini
+start X:\Programs\mariadb\mariadb-10.4.12-winx64\bin\mysqld --defaults-file=X:\Programs\mariadb\mariadb-10.4.12-winx64\data\my.ini
 REM mysql -u root
 cd X:\Programs\nginx\nginx-1.18.0
 start nginx
 cd X:\Projects\HandwerkerFullstack
 X:\Programs\nginx\nginx-1.18.0\start-php-fcgi.bat
+cd X:\Projects\HandwerkerFullstack
 
 
 
