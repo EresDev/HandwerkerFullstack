4993548af2e0a12b761b26ebf3699c7c4d7a9a6e
EresDev <hi@eresdev.com>
2020-05-16 20:49:13 +0500

    updated run command. mysql is not needed to run here.

--- run.bat
+++ run.bat
@@ -1,10 +1,13 @@
 REM Scripts to start programs before development, e.g. start server, mysql, etc.
 
 TASKKILL /F /IM "nginx*"
-X:\Programs\mariadb\mariadb-10.4.12-winx64\bin\mysqladmin -u root shutdown
+REM cd X:\Programs\mariadb\mariadb-10.4.12-winx64\bin\
+REM mysqladmin -u root shutdown
 
 
-start X:\Programs\mariadb\mariadb-10.4.12-winx64\bin\mysqld --defaults-file=X:\Programs\mariadb\mariadb-10.4.12-winx64\data\my.ini
+REM cd X:\Programs\mariadb\mariadb-10.4.12-winx64\bin\
+REM start mysqld --defaults-file=X:\Programs\mariadb\mariadb-10.4.12-winx64\data\my.ini -u root
+
 REM mysql -u root
 cd X:\Programs\nginx\nginx-1.18.0
 start nginx
